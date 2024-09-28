dee8727691f93da0475f4ad02416a8e928fcb17c
Le Thanh <qunthanh1408@gmail.com>
2024-09-28 22:02:22 +0700

    test Netwoking

--- ESP32_2CORE/ESP32_2CORE.ino
+++ ESP32_2CORE/ESP32_2CORE.ino
@@ -5,69 +5,69 @@
 // hàm thu thập thông tin sau đó gửi lên Node-red
 void getDataAndSendToNodeRed()
 {
-    String stringData = "{";
+    // String stringData = "{";
 
-    // nhiet do
-    stringData += "\"nd\": \"";
-    stringData += str_nhietdo;
-    stringData += "\",";
+    // // nhiet do
+    // stringData += "\"nd\": \"";
+    // stringData += str_nhietdo;
+    // stringData += "\",";
 
-    // do am
-    stringData += "\"da\":\"";
-    stringData += str_doam;
-    stringData += "\",";
+    // // do am
+    // stringData += "\"da\":\"";
+    // stringData += str_doam;
+    // stringData += "\",";
 
-    //gas
-    stringData += "\"g\":";
-    stringData += String(buffer_cb_gas);
-    stringData += ",";
+    // //gas
+    // stringData += "\"g\":";
+    // stringData += String(buffer_cb_gas);
+    // stringData += ",";
 
-    //led phai p.khach
-    stringData += "\"lppk\":";
-    stringData += String(ttLedPhaiPK);
-    stringData += ",";
+    // //led phai p.khach
+    // stringData += "\"lppk\":";
+    // stringData += String(ttLedPhaiPK);
+    // stringData += ",";
 
-    //led trai p.khach
-    stringData += "\"ltpk\":";
-    stringData += String(ttLedTraiPK);
-    stringData += ",";
+    // //led trai p.khach
+    // stringData += "\"ltpk\":";
+    // stringData += String(ttLedTraiPK);
+    // stringData += ",";
     
-    //quat p.khach
-    stringData += "\"qpk\":";
-    stringData += String(ttQuatPK);
-    stringData += ",";
+    // //quat p.khach
+    // stringData += "\"qpk\":";
+    // stringData += String(ttQuatPK);
+    // stringData += ",";
 
-    //led p.bep
-    stringData += "\"lpb\":";
-    stringData += String(ttLedBep);
-    stringData += ",";
+    // //led p.bep
+    // stringData += "\"lpb\":";
+    // stringData += String(ttLedBep);
+    // stringData += ",";
 
-    //quat p.bep
-    stringData += "\"qpb\":";
-    stringData += String(ttQuatBep);
-    stringData += ",";
+    // //quat p.bep
+    // stringData += "\"qpb\":";
+    // stringData += String(ttQuatBep);
+    // stringData += ",";
 
-    //ip
-    stringData += "\"ip\":\"";
-    stringData += WiFi.localIP().toString();
-    stringData += "\",";
+    // //ip
+    // stringData += "\"ip\":\"";
+    // stringData += WiFi.localIP().toString();
+    // stringData += "\",";
 
-    unsigned long now = millis();
-    stringData += "\"t\":";
-    stringData += String(now);
+    // unsigned long now = millis();
+    // stringData += "\"t\":";
+    // stringData += String(now);
 
-    stringData += "}";
+    // stringData += "}";
 
-    // Debug
-    Serial.println(stringData);
+    // // Debug
+    // Serial.println(stringData);
 
-    // Publish to MQTT
-    boolean success = sendDataToNodeRed(stringData);
+    // // Publish to MQTT
+    // boolean success = sendDataToNodeRed(stringData);
 
-    if (success)  return;
+    // if (success)  return;
     
-    // Error
-    Serial.println("[MQTT err] Failed to publish to MQTT");
+    // // Error
+    // Serial.println("[MQTT err] Failed to publish to MQTT");
 }
 
 // hàm sử lý lệnh từ Node-red gửi xuống
@@ -82,117 +82,117 @@ void handleCommandFromNodeRed(String cmd){
     //   return;
     // }
 
-    // Led phai p.khach
-    if (cmd == "onlppk")
-    {
-        digitalWrite(LED_PHAI_PKHACH, HIGH);
-        ttLedPhaiPK = 1;
-        Serial.println("." + cmd + ".");      // debug
-        return;
-    }
-    if (cmd == "offlppk")
-    {
-        digitalWrite(LED_PHAI_PKHACH, LOW);
-        ttLedPhaiPK = 0;
-        Serial.println("." + cmd +".");      // debug
-        return;
-    }
+    // // Led phai p.khach
+    // if (cmd == "onlppk")
+    // {
+    //     digitalWrite(LED_PHAI_PKHACH, HIGH);
+    //     ttLedPhaiPK = 1;
+    //     Serial.println("." + cmd + ".");      // debug
+    //     return;
+    // }
+    // if (cmd == "offlppk")
+    // {
+    //     digitalWrite(LED_PHAI_PKHACH, LOW);
+    //     ttLedPhaiPK = 0;
+    //     Serial.println("." + cmd +".");      // debug
+    //     return;
+    // }
 
-    // Led trai p.khach
-    if (cmd == "onltpk")
-    {
-        digitalWrite(LED_TRAI_PKHACH, HIGH);
-        ttLedTraiPK = 1;
-        Serial.println("." + cmd + ".");      // debug
-        return;
-    }
-    if (cmd == "offltpk")
-    {
-        digitalWrite(LED_TRAI_PKHACH, LOW);
-        ttLedTraiPK = 0;
-        Serial.println("." + cmd +".");      // debug
-        return;
-    }
+    // // Led trai p.khach
+    // if (cmd == "onltpk")
+    // {
+    //     digitalWrite(LED_TRAI_PKHACH, HIGH);
+    //     ttLedTraiPK = 1;
+    //     Serial.println("." + cmd + ".");      // debug
+    //     return;
+    // }
+    // if (cmd == "offltpk")
+    // {
+    //     digitalWrite(LED_TRAI_PKHACH, LOW);
+    //     ttLedTraiPK = 0;
+    //     Serial.println("." + cmd +".");      // debug
+    //     return;
+    // }
 
-    // quat p.khach
-    if (cmd == "onqpk")
-    {
-        digitalWrite(QUAT_PKHACH, HIGH);
-        ttQuatPK = 1;
-        Serial.println("." + cmd +".");      // debug
-        return;
-    }
-    if (cmd == "offqpk")
-    {
-        digitalWrite(QUAT_PKHACH, LOW);
-        ttQuatPK = 0;
-        Serial.println("." + cmd +".");      // debug
-        return;
-    }
+    // // quat p.khach
+    // if (cmd == "onqpk")
+    // {
+    //     digitalWrite(QUAT_PKHACH, HIGH);
+    //     ttQuatPK = 1;
+    //     Serial.println("." + cmd +".");      // debug
+    //     return;
+    // }
+    // if (cmd == "offqpk")
+    // {
+    //     digitalWrite(QUAT_PKHACH, LOW);
+    //     ttQuatPK = 0;
+    //     Serial.println("." + cmd +".");      // debug
+    //     return;
+    // }
 
-    // led p.bep
-    if (cmd == "onlpb")
-    {
-        digitalWrite(LED_PBEP, HIGH);
-        ttLedBep = 1;
-        Serial.println("." + cmd +".");      // debug
-        return;
-    }
-    if (cmd == "offlpb")
-    {
-        digitalWrite(LED_PBEP, LOW);
-        ttLedBep = 0;
-        Serial.println("." + cmd +".");      // debug
-        return;
-    }
+    // // led p.bep
+    // if (cmd == "onlpb")
+    // {
+    //     digitalWrite(LED_PBEP, HIGH);
+    //     ttLedBep = 1;
+    //     Serial.println("." + cmd +".");      // debug
+    //     return;
+    // }
+    // if (cmd == "offlpb")
+    // {
+    //     digitalWrite(LED_PBEP, LOW);
+    //     ttLedBep = 0;
+    //     Serial.println("." + cmd +".");      // debug
+    //     return;
+    // }
 
-    // quat p.bep
-    if (cmd == "onqpb")
-    {
-        digitalWrite(QUAT_PBEP, HIGH);
-        ttQuatBep = 1;
-        Serial.println("." + cmd +".");      // debug
-        return;
-    }
-    if (cmd == "offqpb")
-    {
-        digitalWrite(QUAT_PBEP, LOW);
-        ttQuatBep = 0;
-        Serial.println("." + cmd +".");      // debug
-        return;
-    }
+    // // quat p.bep
+    // if (cmd == "onqpb")
+    // {
+    //     digitalWrite(QUAT_PBEP, HIGH);
+    //     ttQuatBep = 1;
+    //     Serial.println("." + cmd +".");      // debug
+    //     return;
+    // }
+    // if (cmd == "offqpb")
+    // {
+    //     digitalWrite(QUAT_PBEP, LOW);
+    //     ttQuatBep = 0;
+    //     Serial.println("." + cmd +".");      // debug
+    //     return;
+    // }
 }
 
 void setup() {
   Serial.begin(115200);
   
-  pinMode(CB_GAS_PIN, INPUT);
-  pinMode(LED_PHAI_PKHACH, OUTPUT);
-  pinMode(LED_TRAI_PKHACH, OUTPUT);
-  pinMode(QUAT_PKHACH, OUTPUT);
-  pinMode(LED_PBEP, OUTPUT);
-  pinMode(QUAT_PBEP, OUTPUT);
+  // pinMode(CB_GAS_PIN, INPUT);
+  // pinMode(LED_PHAI_PKHACH, OUTPUT);
+  // pinMode(LED_TRAI_PKHACH, OUTPUT);
+  // pinMode(QUAT_PKHACH, OUTPUT);
+  // pinMode(LED_PBEP, OUTPUT);
+  // pinMode(QUAT_PBEP, OUTPUT);
 
-  setupWifi();
+  setupWiFi();
   delay(50);
 
   setupMQTT();
   delay(50);
 
-  setupInterrupt();
-  delay(50);
+  // setupInterrupt();
+  // delay(50);
 
-  setupDHT();
-  delay(50);
+  // setupDHT();
+  // delay(50);
 }
 
 void loop() {
-
-  mqtt_client.loop(); 
-
-  xuLyNutNhan();
+  // xuLyNutNhan();
   
-  layGiaTriTuDHT();
+  // layGiaTriTuDHT();
 
-  loopSendReport();
+  // loopSendReport();
+  loopWiFi();
+  
+  loopMqtt();
 }
